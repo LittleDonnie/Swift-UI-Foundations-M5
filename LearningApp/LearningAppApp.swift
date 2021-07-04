94309042a1f211cfa84b2082df86bcf7be1c1660
Monty Montgomery <poweredbyplants@gmail.com>
2021-07-04 17:03:22 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Monty Montgomery on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
