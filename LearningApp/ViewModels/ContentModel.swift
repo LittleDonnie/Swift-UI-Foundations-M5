88376ca73c1305b9fb15683e034b544e5cb87eec
Monty Montgomery <poweredbyplants@gmail.com>
2021-07-06 11:35:44 -0500

    add json data parce json data

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -8,4 +8,35 @@
 import Foundation
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        
+        
+        
+        // Get the path to the json file within the app bundle
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        // get a url to local json file
+        
+        
+        do{
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            self.modules = modules
+        }
+        catch{
+            print("Couldn't parce local data")
+        }
+        // read the file into  data object
+        
+        // try to decode the json into an array of modules
+        
+    }
+    
 }
